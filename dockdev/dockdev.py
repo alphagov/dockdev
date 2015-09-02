6ef44f5bfbd1a19c5d8cf10ce8ea87e1b6916806
Ian Maddison <ian@i.need.stronger.coffee>
2015-09-02 14:58:11 +0100

    Code fine-tuning and tidying. Use OO to make it prettier and more testable

--- dockdev/dockdev.py
+++ dockdev/dockdev.py
@@ -1,99 +1,107 @@
 #!/usr/bin/env python
-import sys, subprocess, os, json, argparse, itertools, git, docker, docker.utils, docker.errors
+import sys, subprocess, os, json, argparse, itertools, git, docker, docker.utils
 
-def docker_args():
+def get_docker():
   kw = docker.utils.kwargs_from_env()
   if kw and 'tls' in kw:
     kw['tls'].assert_hostname = False
-  return kw
+  return docker.Client(**kw)
   
-docker_client = docker.Client(**docker_args())
-
+def get_git():
+  return git.Git()
+  
 def get_config():
-  config_file = os.path.join(os.getcwd(), 'config.json')
+  config_file = os.path.join(os.getcwd(), "config.json")
   with open(config_file) as fp:
-    return json.loads(fp.read())
+    return parse_config(fp.read())
 
-def get_commit(repo, branch):
-  commit = git.Git().ls_remote(repo, branch).split("\t")[0]
-  if commit == '':
-    raise Exception("Branch %s not found in %s" % (branch, repo))
-  return commit
+def parse_config(config):
+  config_json = json.loads(config)
+  services = []
+  for name, config in config_json["services"].iteritems():
+    merged_config = config_json.get("template", {})
+    merged_config.update(config)
+    merged_config = {k: os.path.expandvars(v.format(name=name)) for k, v in merged_config.iteritems()}
+    services.append(Service(name, merged_config))
+  return services
+
+def get_value(config, key):
+  return config.get("docker_repo", template.get("docker_repo", None))
+
+class Service(object):
+  def __init__(self, name, config):
+    self.name = name
+    self.docker_repo = config.get("docker_repo", None)
+    self.git_repo = config.get("git_repo", None)
+    self.build_dir = config.get("build_dir", None)
+    
+  def build(self, git, auto_checkout):
+    if not os.path.exists(self.build_dir):
+      if auto_checkout:
+        print "[{s.name}] cloning git repo {s.git_repo} to {s.build_dir}".format(s=self)
+        git.clone(self.git_repo, self.build_dir)
+      else:
+        raise Exception ("{s.build_dir} does not exist. Either checkout or run with -c.".format(s=self))
+    
+    build_script = os.path.join(self.build_dir, "build-local.sh")
+    print "[{s.name}] Building using {script}...".format(s=self, script=build_script)
+    p = subprocess.Popen(build_script, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=self.build_dir, bufsize=0)
+    while True:
+      line = p.stdout.readline()
+      if line != '':
+        print "[{s.name}] {line}".format(s=self, line=line.rstrip())
+      else:
+        break
+    exit_code = p.wait()
+    if exit_code != os.EX_OK:
+      raise Exception("Stopped due to build error: {err}".format(err=exit_code))
+      
+    print "[{s.name}] {s.docker_repo}:local build successful".format(s=self)
   
-def fetch_image(config, app, branch):
-  repo = config['hub'] % app
-  head = get_commit(config["git"] % app, branch)
+  def fetch(self, branch, docker):
+    head = git.Git().ls_remote(self.git_repo, branch).split("\t")[0]
+    if not head:
+      raise Exception("Branch {branch} not found in {s.git_repo}".format(s=self, branch=branch))
+    
+    print "[{s.name}] {branch} HEAD is {head}".format(s=self, head=head, branch=branch)
+    print "[{s.name}] fetching {s.docker_repo}:{head}".format(s=self, head=head)
+    docker.pull(self.docker_repo, tag=head)
+    print "[{s.name}] tagging fetched image as {s.docker_repo}:local".format(s=self)
+    docker.tag(self.docker_repo + ":" + head, self.docker_repo, "local", True)
+
+def parse_args(argv):
+  parser = argparse.ArgumentParser(
+    usage="Fetches and/or builds docker images for services and tags them with :local in your machine's docker. You " +
+      "can then use them in scenarios like docker-compose to start an app comprised of multiple services. Default is " +
+      "fetching master images from the registry, but can also fetch named branches or build locally with any changes. ")
+  parser.add_argument('-l', '--local', default=[], action='append', nargs=1,
+    metavar="APP[,APP[,...]]", help="Build service image for local git checkout (inc. changes)")
+  parser.add_argument('-b', '--branch', default=[], action='append', nargs=2,
+    metavar=("BRANCH", "APP[,APP[,...]]"), help="Fetch service image for built for branch")
+  parser.add_argument('-o', '--only', default=False, action='store_true',
+    help="Turns off default master fetch. Only build services explicitly specified in options.")
+  parser.add_argument('-c', '--checkout', default=False, action='store_true',
+    help="Enable automatic checkout of services if not currently present in build dir.") 
+    
+  parsed_args = parser.parse_args(argv)
+  local = set(itertools.chain(*[x[0].split(',') for x in parsed_args.local]))
+  branch = dict(itertools.chain(*map(lambda e : [(key, e[0]) for key in e[1].split(',')], parsed_args.branch)))
   
-  print "[%s] %s HEAD is %s, fetching %s:%s" % (app, branch, head, repo, head)
-  docker_client.pull(repo, tag=head)
-  
-  print "[%s] tagging %s:%s as %s:local" % (app, repo, head, repo)
-  docker_client.tag(repo + ":" + head, repo, "local", True)
-  
-def build_image(config, app, checkout):
-  repo = config['hub'] % app
-  build_dir = os.path.expandvars(config['dir'] % app)
-  if not os.path.exists(build_dir):
-    if checkout:
-      git_repo = config["git"] % app
-      print "[%s] cloning git repo %s in to %s" % (app, git_repo, build_dir)
-      git.Git().clone(git_repo, build_dir)
-    else:
-      raise Exception ("%s does not exist. Either checkout or run with -c." % build_dir)
-  
-  build_script = os.path.join(build_dir, "build-local.sh")
-  
-  print "[%s] Building using %s..." % (app, build_script)
-  p = subprocess.Popen(build_script, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=build_dir, bufsize=0)
-  while True:
-    line = p.stdout.readline()
-    if line != '':
-      print "[%s] %s" % (app, line.rstrip())
-    else:
-      break
-  exit_code = p.wait()
-  if exit_code != os.EX_OK:
-    raise Exception("Stopped due to build error: %s" % str(exit_code))
-  
-  print "[%s] %s:local build finished" % (app, repo)
+  return (local, branch, parsed_args.checkout, parsed_args.only)
 
 def main(argv):
-  parser = argparse.ArgumentParser(
-    usage="Fetches or builds docker images for microservices and tags them with :local in " +
-          "your running docker instance so docker-compose will use them when spinning up a " +
-          "development environment. Defaults to fetching master images from DockerHub, can " +
-          "also fetch particular branches or build checked out source with the following " +
-          "options.")
-  parser.add_argument('-l', '--local', default=[], action='append', nargs=1,
-    metavar="APP[,APP[,...]]", help="Build app image for local git checkout (inc. changes)")
-  parser.add_argument('-b', '--branch', default=[], action='append', nargs=2,
-    metavar=("BRANCH", "APP[,APP[,...]]"), help="Fetch app image for built for branch")
-  parser.add_argument('-o', '--only', default=False, action='store_true',
-    help="Turns off default master fetch. Only build apps explicitly specified in options.")
-  parser.add_argument('-c', '--checkout', default=False, action='store_true',
-    help="Enable automatic checkout of projects if not currently present in build dir (experimental).")
-
-  args = parser.parse_args(argv)
-  config = get_config()
-  local = set(itertools.chain(*[x[0].split(',') for x in args.local]))
-  branch = dict(itertools.chain(*map(lambda e : [(key, e[0]) for key in e[1].split(',')], args.branch)))
-  only = args.only
-
-  for app in config['apps']:
-    if app in local:
-      build_image(config, app, args.checkout)
-    elif app in branch:
-      fetch_image(config, app, branch[app])
+  docker = get_docker()
+  git = get_git()
+  (local, branch, auto_checkout, only) = parse_args(argv)
+  
+  print "----------------------------------------"
+  for service in get_config():
+    if service.name in local:
+      service.build(git, auto_checkout)
+    elif service.name in branch:
+      service.fetch(branch[service.name], docker)
     elif not only:
-      fetch_image(config, app, 'master')
+      service.fetch("master", docker)
     else:
-      print "[%s] skipped" % app
+      print "[{s.name}] skipped".format(s=service)
     print "----------------------------------------"
-
-if __name__ == "__main__":
-  try:
-    main(sys.argv[1:])
-  except KeyboardInterrupt as e:
-    print "Aborted"
-  except Exception as e:
-    print "ERROR: %s" % str(e)
