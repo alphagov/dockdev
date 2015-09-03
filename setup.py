3c9c1bfa4313dc432f1220efa3e02b4cb2ebcc1e
Ian Maddison <ian@i.need.stronger.coffee>
2015-09-03 21:50:46 +0100

    Bump version for release

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
-import os
 from setuptools import setup
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "dockdev",
-    version = "0.1.1",
+    version = "0.2.0",
     author = "Ian Maddison",
     author_email = "ian.maddison@digital.cabinet-office.gov.uk",
     description = ("A simple development setup tool for docker containerised apps"),
@@ -16,7 +12,7 @@ setup(
     packages=['dockdev'],
     scripts=['bin/dockdev'],
     install_requires=['docker-py>=1.3.1', 'GitPython>=1.0.1'],
-    long_description=read('README.md'),
+    long_description='See https://github.com/alphagov/dockdev/README.md',
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Software Development :: Build Tools",
